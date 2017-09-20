4d263eeb7012adf840b52a2b8a7ab02171b7d7f4
Oliver <oc@hejverden.dk>
2017-09-20 14:03:17 +0200

    Add methods to navigate the dungeon.

--- text_dungeon.rb
+++ text_dungeon.rb
@@ -8,11 +8,48 @@ class Dungeon
     @rooms = []
   end
 
-  Player = Struct.new(:name, :location)
-  Room = Struct.new(:reference, :name, :description, :connections)
-
   def add_room(reference, name, description, connections)
     @rooms << Room.new(reference, name, description, connections)
+  end
+
+  def start(location)
+    @player.location = location
+    show_current_description 
+  end
+
+  def show_current_description
+    find_room_in_dungeon(@player.location).full_description
+  end
+
+  def find_room_in_dungeon(reference)
+    @rooms.detect { |room| room.reference == reference }
+  end
+
+  def find_room_in_direction(direction)
+    find_room_in_dungeon(@player_location).connections[direction]
+  end
+
+  def go(direction)
+    puts "You go #{direction}"
+    player_location = find_room_in_dungeon(direction)
+    show_current_description
+  end
+
+  Player = Struct.new(:name, :location)
+
+  class Room
+    attr_accessor :reference, :name, :description, :connections
+
+    def initialize(reference, name, description, connections)
+      @reference = reference
+      @name = name
+      @description = description
+      @connections = connections
+    end
+
+    def full_description
+      "#{@name}\n\nYou are in #{@description}"
+    end
   end
 
 end
